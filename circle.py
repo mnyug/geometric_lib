edadb031d0c9580948e5a1e651e8b5b286ca746d
mnyug <krskkungg@gmail.com>
2025-10-27 23:14:47 +0300

    edit: readme.md & circle.py & square.py | add: rectangle.py & triangle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,14 @@ import math
 
 
 def area(r):
+    """r — радиус круга.
+    Вычисляет площадь круга.
+    """
     return math.pi * r * r
 
 
 def perimeter(r):
+    """r — радиус круга.
+    Вычисляет периметр круга.
+    """
     return 2 * math.pi * r
-
